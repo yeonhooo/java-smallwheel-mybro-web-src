b08666901aa7881174a6a3a3e891785c122ec4b8
yeonho <yeonho@DESKTOP-5882K9E>
2016-08-04 10:23:44 +0900

    Windows일 경우, 구동 시 default browser를 통해 app을 띄워준다.

--- mybro-web/src/main/java/smallwheel/mybro/App.java
+++ mybro-web/src/main/java/smallwheel/mybro/App.java
@@ -1,5 +1,8 @@
 package smallwheel.mybro;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
@@ -9,8 +12,12 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class App extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-    
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(App.class, args);
+
+		if (System.getProperty("os.name").startsWith("Windows")) {
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://" + InetAddress.getLocalHost().getHostAddress());
+		}
+	}
+
 }
