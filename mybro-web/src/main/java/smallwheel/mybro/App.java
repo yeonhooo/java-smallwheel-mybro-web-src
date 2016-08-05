4e5a1258f7440b0982964a265579ac2d870b9678
Donghwan Lee <colus4@gmail.com>
2016-08-05 19:08:02 +0900

    브라우저 자동 실행 macOS 추가

--- mybro-web/src/main/java/smallwheel/mybro/App.java
+++ mybro-web/src/main/java/smallwheel/mybro/App.java
@@ -3,22 +3,29 @@ package smallwheel.mybro;
 import java.io.IOException;
 import java.net.InetAddress;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
 public class App extends SpringBootServletInitializer {
-
+	
 	public static void main(String[] args) throws IOException {
-		SpringApplication.run(App.class, args);
-
-		if (System.getProperty("os.name").startsWith("Windows")) {
-			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://" + InetAddress.getLocalHost().getHostAddress());
-		}
+		ApplicationContext ctx = SpringApplication.run(App.class, args);
 		
-	}
+		String port = StringUtils.defaultIfEmpty( ctx.getEnvironment().getProperty("server.port"), "8080");
+		String url = String.format("http://%s%s", InetAddress.getLocalHost().getHostAddress(), ( port != null ? ":" + port : "" ) );
 
+		if ( SystemUtils.IS_OS_WINDOWS ) {
+			Runtime.getRuntime().exec(String.format("rundll32 url.dll,FileProtocolHandler %s",  url));
+		} else if ( SystemUtils.IS_OS_MAC ) {
+			Runtime.getRuntime().exec(String.format("open %s",  url));
+		}
+	}
 }
