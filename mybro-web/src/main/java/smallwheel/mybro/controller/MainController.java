02522963499052379e9a4f73f42296e727ce9077
yeonho <yeonho@10.255.203.70>
2017-03-09 20:56:14 +0900

    zip 관련 변경

--- mybro-web/src/main/java/smallwheel/mybro/controller/MainController.java
+++ mybro-web/src/main/java/smallwheel/mybro/controller/MainController.java
@@ -19,6 +19,7 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -66,6 +67,14 @@ public class MainController {
     	return null;
     }
     
+    /**
+     * 생성한 결과 파일들을 압축하여 zip 파일로 다운로드한다.
+     * 
+     * @param session
+     * @param response
+     * @return
+     * @throws IOException
+     */
     @RequestMapping(value = "/api/tables/zip", method = RequestMethod.GET, produces="application/zip")
 	public @ResponseBody byte[] zipFiles(HttpSession session, HttpServletResponse response) throws IOException {
     	
@@ -103,6 +112,80 @@ public class MainController {
 		IOUtils.closeQuietly(bufferedOutputStream);
 		IOUtils.closeQuietly(byteArrayOutputStream);
 		return byteArrayOutputStream.toByteArray();
+	}
+
+    /**
+     * 생성한 결과 파일들을 압축하여 zip 파일로 다운로드한다.
+     * 
+     * @param session
+     * @param response
+     * @return
+     * @throws IOException
+     */
+	@RequestMapping(value = "/api/tables/zip-dir", method = RequestMethod.GET, produces = "application/zip")
+	public @ResponseBody byte[] zipFiles_Dir(HttpSession session, HttpServletResponse response) throws IOException {
+
+		// setting headers
+		response.setContentType("application/zip");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + session.getId() + ".zip\"");
+		response.addHeader(HttpHeaders.CONNECTION, "close");
+
+		// creating byteArray stream, make it bufforable and passing this buffor to ZipOutputStream
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(byteArrayOutputStream);
+		ZipOutputStream zipOutputStream = new ZipOutputStream(bufferedOutputStream);
+
+		// file list
+		String srcDirPath = new File(Path.RESULT_DES_DIR + session.getId()).getAbsolutePath();
+		addDirectory(zipOutputStream, srcDirPath, srcDirPath);
+
+		if (zipOutputStream != null) {
+			zipOutputStream.finish();
+			zipOutputStream.flush();
+			IOUtils.closeQuietly(zipOutputStream);
+		}
+
+		IOUtils.closeQuietly(bufferedOutputStream);
+		IOUtils.closeQuietly(byteArrayOutputStream);
+		return byteArrayOutputStream.toByteArray();
+	}
+
+	private void addDirectory(ZipOutputStream zos, String folderName, String baseFolderName) throws IOException {
+		File f = new File(folderName);
+
+		if (f.exists()) {
+			if (f.isDirectory()) {
+				// For pointing out missing entry for empty folder
+				if (!folderName.equalsIgnoreCase(baseFolderName)) {
+					String entryName = folderName.substring(baseFolderName.length() + 1, folderName.length()) + File.separatorChar;
+					LOGGER.info("Adding folder entry " + entryName);
+					ZipEntry ze = new ZipEntry(entryName);
+					zos.putNextEntry(ze);
+				}
+				File f2[] = f.listFiles();
+				for (int i = 0; i < f2.length; i++) {
+					addDirectory(zos, f2[i].getAbsolutePath(), baseFolderName);
+				}
+			} else {
+				// add file
+				// extract the relative name for entry purpose
+				String entryName = folderName.substring(baseFolderName.length() + 1, folderName.length());
+				LOGGER.info("Adding file entry " + entryName);
+				ZipEntry ze = new ZipEntry(entryName);
+				zos.putNextEntry(ze);
+				FileInputStream in = new FileInputStream(folderName);
+				int len;
+				byte buffer[] = new byte[1024];
+				while ((len = in.read(buffer)) > 0) {
+					zos.write(buffer, 0, len);
+				}
+				in.close();
+				zos.closeEntry();
+			}
+		} else {
+			LOGGER.info("File or directory not found " + folderName);
+		}
 	}
     
 }
